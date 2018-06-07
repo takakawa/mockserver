8c02daa364474e6d9fd9b1e20fc77b6683cafce6
takakawa <takakawa@163.com>
2018-06-07 19:48:35 +0800

    bakup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::net::{TcpListener,TcpStream};
+use std::thread;
+use std::io;
+use std::io::{Read,Write};
+
+fn handle_write(mut stream: &TcpStream) -> io::Result<usize>{
+	let resp = b"test,test,test";
+	match stream.write(resp){
+		Ok(o)=>{println!("Sent ok,{}",o);Ok(o)},
+		Err(e)=>{println!("Failed to send:{}",e);Err(e)},
+	}
+}
+
+fn handle_read(mut  stream: &TcpStream) -> io::Result<usize>{
+	let mut buf  = [0u8, 4096];
+	let ret = match stream.read(& mut buf){
+		Ok(o)=>{
+			let req_str = String::from_utf8_lossy(&buf);
+			println!("{}",req_str);
+                        Ok(o);
+		},
+		Err(e)=> {
+			println!("Unable to read stream: {}",e);
+			Err(e);
+		},
+	};
+	ret;
+}
+fn handle_client(stream: TcpStream){
+	println!("handling tcpstream");
+        loop {
+		handle_read(&stream);
+		handle_write(&stream);
+	}
+}
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:19890").unwrap();
+    println!("Listening for conections on port {}",19890);
+
+    for stream in listener.incoming() {
+      match stream {
+          Ok(conn) => {
+			thread::spawn(||{handle_client(conn)});
+		},
+	  Err(e) => {
+		println!("Unable to connect:{}",e);
+          }
+      }
+    }
 }
